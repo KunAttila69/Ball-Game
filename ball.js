5564eee5a5d07f0580d781211e634e5869f670cb
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2022-09-14 13:53:51 +0200

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,46 @@
+class Ball{
+    constructor(x,y,speed,size,color,dirX,dirY,gravity,velocity){
+        this.x = x,
+        this.y = y,
+        this.speed = speed,
+        this.size = size,
+        this.color = color,
+        this.dirX = dirX,
+        this.dirY = dirY
+        this.gravity = gravity
+        this.velocity = velocity
+    }
+}
+
+function CreateBall(ballX,ballY,ballSpeed,ballSize,ballColor,ballDirX,ballDirY,gravity,velocity){
+    return new Ball(ballX,ballY,ballSpeed,ballSize,ballColor,ballDirX,ballDirY,gravity,velocity)
+}
+
+function DrawBall(ball){
+    console.log(ball)
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, 2 * Math.PI);
+    ctx.fillStyle = ball.color
+    ctx.fill();
+    ctx.stroke();
+}
+
+function MoveBall(ball){
+    ball.speed += ball.velocity
+    if(ball.x >= canvas.width){
+        ball.dirX *= -1;
+    }
+    else if(ball.x < 0){
+        ball.dirX *= -1;
+    }
+    ball.x += ball.speed*ball.dirX;
+    
+    if(ball.y > canvas.height){
+        ball.dirY *= -1;
+        ball.speed /= ball.gravity
+    }
+    else if(ball.y < 0){
+        ball.dirY *= -1;
+    }
+    ball.y += ball.speed*ball.gravity*ball.dirY;
+  }
---
+++ main.js
@@ -0,0 +1,15 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext('2d');
+var ball = CreateBall(canvas.width/2,canvas.height/2,3,10,"red",1,1,10, 0.1)
+
+
+function Animate(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    DrawBall(ball)
+    MoveBall(ball)
+    requestAnimationFrame(Animate);
+}
+Animate();
